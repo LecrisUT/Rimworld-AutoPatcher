30bf6ce3573990040cb9586693aec2c4f81c516b
LecrisUT <lecris.phy@gmail.com>
2020-07-19 14:38:11 +0900

    Generic Def fix
    Rimworld gives error if a generic Def is used due to DefDatabse creation

---
+++ Source/AutoPatcher/MainMod.cs
@@ -0,0 +1,18 @@
+﻿using HarmonyLib;
+using Verse;
+
+namespace AutoPatcher
+{
+    public sealed class MainMod : Mod
+    {
+        public static ModContentPack ourContentPack;
+        public static Harmony harmony;
+        public MainMod(ModContentPack content) : base(content)
+        {
+            // Early patch to avoid generic Def issues
+            harmony = new Harmony("AutoPatcher");
+            harmony.Patch(AccessTools.Method(typeof(GenTypes), "AllSubclasses"),
+                postfix: new HarmonyMethod(typeof(Patch_GenTypes_AllSubclasses), "Postfix"));
+        }
+    }
+}
