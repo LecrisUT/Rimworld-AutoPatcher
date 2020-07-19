c30ea1849dd9c6dbb6f605361ede8fe1da91e31b
LecrisUT <lecris.phy@gmail.com>
2020-07-19 14:45:25 +0900

    NodeUtility

---
+++ Source/AutoPatcher/NodeUtility.cs
@@ -0,0 +1,20 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using Verse;
+
+namespace AutoPatcher.Utility
+{
+    /// <summary>
+    /// Helper functions
+    /// </summary>
+    public static class NodeUtility
+    {
+        public static List<NodeDef> allNodeDefs = new List<NodeDef>();
+        private static Type thisType = typeof(NodeUtility);
+        public static MethodInfo getDefListInfo = AccessTools.Method(thisType, "GetDefList");
+        public static List<Def> GetDefList<T>() where T : Def
+            => DefDatabase<T>.AllDefsListForReading.ConvertAll(t => (Def)t);
+    }
+}
