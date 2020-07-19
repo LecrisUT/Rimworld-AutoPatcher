1d61bfd1fd409d637d92057da9e493a2c469986f
LecrisUT <lecris.phy@gmail.com>
2020-07-19 14:43:09 +0900

    Basic node class

---
+++ Source/AutoPatcher/Def/NodeBases/BeginNode.cs
@@ -0,0 +1,17 @@
+﻿namespace AutoPatcher
+{
+    public abstract class BeginNode<T> : NodeDef where T : class
+    {
+        public BeginNode()
+        {
+            baseInPorts = 0;
+            baseOutPorts = 1;
+            nOutPortGroups = 1;
+            nInPortGroups = 0;
+            nOutPorts = nOutPortGroups * baseOutPorts;
+            nInPorts = nInPortGroups * baseInPorts;
+        }
+        protected override void CreateOutputPortGroup(Node node, int group)
+            => node.outputPorts.Add(new Port<T>());
+    }
+}
