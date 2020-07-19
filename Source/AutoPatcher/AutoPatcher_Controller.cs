3e2e4cfa8efccfb7985fd034af4a7d63491bcacf
LecrisUT <lecris.phy@gmail.com>
2020-07-19 14:46:25 +0900

    Controller class
    Executes the patch trees on DefsLoaded

---
+++ Source/AutoPatcher/AutoPatcher_Controller.cs
@@ -0,0 +1,23 @@
+﻿using AutoPatcher.Utility;
+using HarmonyLib;
+using HugsLib;
+using System.Collections.Generic;
+using Verse;
+
+namespace AutoPatcher
+{
+    internal class Controller : ModBase
+    {
+        public override void DefsLoaded()
+        {
+            // Run patch trees
+            List<PatchTreeDef> patchTrees = DefDatabase<PatchTreeDef>.AllDefsListForReading;
+            NodeUtility.allNodeDefs.Do(t => t.Initialize());
+            foreach (PatchTreeDef patchTree in patchTrees)
+            {
+                patchTree.Initialize();
+                patchTree.Perform();
+            }
+        }
+    }
+}
